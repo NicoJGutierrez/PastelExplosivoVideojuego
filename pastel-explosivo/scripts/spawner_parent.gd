5c373c7a8898db7d7e538bb2c30557af1ad81590
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2025-10-14 23:14:10 -0300

    feat: spawners funcionando

--- pastel-explosivo/scripts/spawner_parent.gd
+++ pastel-explosivo/scripts/spawner_parent.gd
@@ -1,1 +1,15 @@
 extends Node3D
+
+var hijos
+var hijo_spawneando = 0
+
+func _ready():
+	hijos = get_children()
+
+func _on_timer_timeout():
+	var hijo = hijos[hijo_spawneando]
+	if hijo.has_method("spawnear"):
+		hijo.spawnear()
+	hijo_spawneando += 1
+	if hijo_spawneando >= len(hijos):
+		hijo_spawneando = 0
