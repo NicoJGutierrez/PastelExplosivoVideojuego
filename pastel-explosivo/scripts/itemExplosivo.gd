d2596bb329552295b530bf7a4387f35011179659
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2025-10-19 16:22:17 -0300

    feat: grupos actualizados

--- pastel-explosivo/scripts/itemExplosivo.gd
+++ pastel-explosivo/scripts/itemExplosivo.gd
@@ -10,19 +10,26 @@ func _ready():
 func _physics_process(delta):
 	if self.get_parent().get_parent() != null and self.get_parent().get_parent().is_in_group("player"):
 		for node in get_colliding_bodies():
-			if node.is_in_group("player") and node != get_parent().get_parent():
-				explode(3)
-			elif node.is_in_group("pickuplocal"):
-				explode(4)
-			elif not node.is_in_group("player"):
-				current_boom += 1
+			if is_instance_valid(node):
+				if node.is_in_group("player") and node != get_parent().get_parent():
+					print("2")
+					explode(3)
+				elif node.is_in_group("ingrediente"):
+					pass
+				elif node.is_in_group("pickuplocal"):
+					print("1")
+					explode(4)
+				else: 
+					if not node.is_in_group("player"):
+						current_boom += 1
 		if current_boom == chance_to_boom:
 				current_boom = 0
+				print("3")
 				explode(2)
 			
 func explode(force):
 	var nueva_explosion = preload("res://scripts/explosion.tscn").instantiate()
-	
+	print("bum")
 	nueva_explosion.force = force
 	
 	get_tree().root.add_child(nueva_explosion)
